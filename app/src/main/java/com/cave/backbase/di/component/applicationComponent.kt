a3abeea83803aeb910fbae39b83e0ae949e0b596
keyvan <keyvan.nrz@gmail.com>
2022-07-17 19:17:50 +0430

    add viewModelModule

--- app/src/main/java/com/cave/backbase/di/component/applicationComponent.kt
+++ app/src/main/java/com/cave/backbase/di/component/applicationComponent.kt
@@ -1,8 +1,10 @@
 package com.cave.backbase.di.component
 
 import com.cave.backbase.di.modules.applicationModule
+import com.cave.backbase.di.modules.viewModelModule
 import org.koin.core.module.Module
 
 val applicationComponent: List<Module> = listOf(
-    applicationModule
+    applicationModule,
+    viewModelModule
 )
