bc3ae36479d102552c8d27efd486e3949c84d424
keyvan <keyvan.nrz@gmail.com>
2022-07-17 19:40:34 +0430

    Implement MainActivity and MainViewModel from baseClases

--- app/src/main/java/com/cave/backbase/ui/MainActivity.kt
+++ app/src/main/java/com/cave/backbase/ui/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.cave.backbase.ui
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.cave.backbase.R
+import android.view.LayoutInflater
+import com.cave.backbase.base.BaseActivity
+import com.cave.backbase.databinding.ActivityMainBinding
+import kotlin.reflect.KClass
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+    override val bindingInflater: (LayoutInflater) -> ActivityMainBinding =
+        ActivityMainBinding::inflate
+
+    override fun getViewModelJavaClass(): KClass<MainViewModel> = MainViewModel::class
 }
